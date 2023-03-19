f486424d218c96d69f39bbd446a525f480b5d979
Roman <112611445+Makar789654@users.noreply.github.com>
2023-03-19 20:11:33 +0100

    Add files via upload

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,19 +1,39 @@
-import React from 'react';
-import { useState , useEffect} from "react";
-import {Box , Stack , Typography} from "@mui/material";
-import SideBar from './SideBar';
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Videos, Sidebar } from "./";
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState(null);
+
+  useEffect(() => {
+    setVideos(null);
+
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
+    }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-        <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "2px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-        <SideBar />
+      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+        
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
-          Copyright © 2023 MAKAR Media
+          Copyright © 2023 Makar Media
         </Typography>
-        </Box>
-    </Stack>
-  )
-}
+      </Box>
 
-export default Feed
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+          {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
+        </Typography>
+
+        <Videos videos={videos} />
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
